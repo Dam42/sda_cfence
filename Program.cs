163e015781905d6835f18c6b6b50e3332218fe25
Dam42 <Dam42@users.noreply.github.com>
2022-05-14 10:27:19 +0200

    final verison with everything working

--- Program.cs
+++ Program.cs
@@ -4,13 +4,13 @@ namespace sda_cfence
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Insert a number and power to which you want it to be raised");
             int num = Convert.ToInt32(Console.ReadLine());
             int pow = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine(Power(num, pow)); 
+            if (pow < 0) Console.WriteLine("potęga nie może być niższa od 0 bo nie ogarniam");
+            else Console.WriteLine(Power(num, pow)); 
         }
 
         static int Power(int num, int pow)
