d627543fe5398f10a6a008deeae713b85d2fce6a
Dam42 <Dam42@users.noreply.github.com>
2022-05-14 14:43:19 +0200

    bruh moment

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace sda_cfence
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string pesel = "55030101193";
+
+            Console.WriteLine(CheckControlNumber(pesel));
+        }
+
+        static bool CheckControlNumber(string pesel)
+        {
+            int[] checks = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 ,0 };
+            int[] numbers = new int[11];
+            int lastNum = Convert.ToInt32(Convert.ToString(pesel[10]));
+            int sum = 0;
+            int i = 0;
+
+            foreach(char c in pesel)
+            {
+                numbers[i] = Convert.ToInt32(Convert.ToString(c));
+                numbers[i] *= checks[i];
+                sum += numbers[i];
+                i++;
+            }
+
+            int M = sum % 10;
+            if( ((10 - M) == lastNum) || (M == 0  && 0 == lastNum)) return true;
+            return false;
         }
     }
 }
