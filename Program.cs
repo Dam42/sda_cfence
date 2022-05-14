d4120f951793e078d11eb7d137a12af647193902
Dam42 <Dam42@users.noreply.github.com>
2022-05-14 10:07:40 +0200

    e02 done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace sda_cfence
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Insert numamber and power to which you want it to be raised");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int pow = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Power(num, pow)); 
+        }
+
+        static int Power(int num, int pow)
+        {
+            int result = num;
+            while (pow > 1) {
+                result *= num;
+                pow--;
+            }
+            return result;
         }
     }
 }
