dac528cf971a6050b3846eb95f742aabc0ddb99d
Dam42 <Dam42@users.noreply.github.com>
2022-05-14 10:00:32 +0200

    zadanie e01 done

--- Program.cs
+++ Program.cs
@@ -2,11 +2,30 @@
 
 namespace sda_cfence
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Podaj cenę produktu:");
+                int price = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Podaj liczbę produktów:");
+                int productNumber = Convert.ToInt32(Console.ReadLine());
+                PriceList(price, productNumber);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void PriceList(int cena, int liczba)
+        {
+            for (int i = 1; i <= liczba; i++)
+            {
+                Console.WriteLine($"Liczba sztuk {i} łaczna cena {cena * i}");
+            }
         }
     }
 }
