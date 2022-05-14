e28758c40058096b89b0a1a3db03f17531f396c8
Dam42 <Dam42@users.noreply.github.com>
2022-05-14 10:57:10 +0200

    e04 done damn that was kinda hard or I'm just retarded

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace sda_cfence
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            triangle("*", 7);
+        }
+
+        static void triangle(string symbol, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < i; j++) Console.Write(symbol);
+                Console.WriteLine();
+            }
+            for (int i = size; i > 0; i--)
+            {
+                for (int j = i; j > 0; j--) Console.Write(symbol);
+                Console.WriteLine();
+            }
         }
     }
 }
