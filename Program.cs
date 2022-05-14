dbdaf2cc5ebce105576dd848f6cd96ff42f3f8d9
Dam42 <Dam42@users.noreply.github.com>
2022-05-14 11:33:22 +0200

    done e05

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace sda_cfence
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Factorial(5));
+
+            Console.WriteLine(FactorialnotRecursive(5));
+        }
+
+        static int Factorial(int num)
+        {
+            if (num == 0) return 1;
+            return num * Factorial(num - 1);
+        }
+
+        static int FactorialnotRecursive(int num)
+        {
+            int result = 1;
+            for (int i = 1; i <= num; i++) result *= i;
+            return result;
         }
     }
 }
