df527269954235f1f4e1e48abb30828cfe5dc45d
Dam42 <Dam42@users.noreply.github.com>
2022-05-14 12:02:07 +0200

    e06 done with some help form stackoverflow

--- Program.cs
+++ Program.cs
@@ -4,9 +4,36 @@ namespace sda_cfence
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string pass = "1qazXSW#";
+
+            Console.WriteLine(Verify(pass)); 
         }
+
+        static bool Verify(string pass)
+        {
+            if ( IsLong(pass) && HasNumber(pass) && HasSpecial(pass)) return true;
+            return false;
+        }
+
+        static bool IsLong(string pass)
+        {
+            if (pass.Length >= 6) return true;
+            return false;
+        }
+        static bool HasNumber(string pass)
+        {
+            char[] chPass = pass.ToCharArray();
+            foreach (char c in chPass) if (char.IsDigit(c)) return true;
+            return false;
+        }
+        static bool HasSpecial(string pass)
+        {
+            char[] chars = { '&', '*', '$', '#' };
+            if (pass.IndexOfAny(chars) == -1) return false;
+            return true;
+        }
+
     }
 }
