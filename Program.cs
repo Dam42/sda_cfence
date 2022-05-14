fb3d1b9f76066baaeda53759c14f27d6af44ae4d
Dam42 <Dam42@users.noreply.github.com>
2022-05-14 12:08:51 +0200

    code maid

--- Program.cs
+++ Program.cs
@@ -2,50 +2,53 @@
 
 namespace sda_cfence
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        private static void Main()
         {
             string pass = "1qazXSW#";
 
-            Console.WriteLine(Verify(pass)); 
+            Console.WriteLine(Verify(pass));
         }
 
-        static bool Verify(string pass)
+        private static bool Verify(string pass)
         {
-            if ( IsLong(pass) && HasNumber(pass) && HasSpecial(pass) && HasUpperCase(pass) && HasLowerCase(pass)) return true;
+            if (IsLong(pass) && HasNumber(pass) && HasSpecial(pass) && HasUpperCase(pass) && HasLowerCase(pass)) return true;
             return false;
         }
 
-        static bool IsLong(string pass)
+        private static bool IsLong(string pass)
         {
             if (pass.Length >= 6) return true;
             return false;
         }
-        static bool HasNumber(string pass)
+
+        private static bool HasNumber(string pass)
         {
             char[] chPass = pass.ToCharArray();
             foreach (char c in chPass) if (char.IsDigit(c)) return true;
             return false;
         }
-        static bool HasUpperCase(string pass)
+
+        private static bool HasUpperCase(string pass)
         {
             char[] chPass = pass.ToCharArray();
             foreach (char c in chPass) if (char.IsUpper(c)) return true;
             return false;
         }
-        static bool HasLowerCase(string pass)
+
+        private static bool HasLowerCase(string pass)
         {
             char[] chPass = pass.ToCharArray();
             foreach (char c in chPass) if (char.IsLower(c)) return true;
             return false;
         }
-        static bool HasSpecial(string pass)
+
+        private static bool HasSpecial(string pass)
         {
             char[] chars = { '&', '*', '$', '#' };
             if (pass.IndexOfAny(chars) == -1) return false;
             return true;
         }
-
     }
 }
