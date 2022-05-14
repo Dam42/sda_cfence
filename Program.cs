b9d6e60aa55be05c3f5f4934211770268bf9ee80
Dam42 <Dam42@users.noreply.github.com>
2022-05-14 14:04:50 +0200

    make a horibble solution

--- Program.cs
+++ Program.cs
@@ -2,11 +2,21 @@
 
 namespace sda_cfence
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = { 2, 5, 1, 2, 5};
+
+            RemoveDups(numbers);
+        }
+
+        private static void RemoveDups(int[] numbers)
+        {
+            int[] tempNumbers = new int[6];
+            foreach (int num in numbers) tempNumbers[num] = 1;
+
+            for (int i = 0; i < tempNumbers.Length; i++) if (tempNumbers[i] == 1) Console.Write($"{i} ");
         }
     }
 }
